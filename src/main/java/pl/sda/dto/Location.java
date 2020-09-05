a614e5342532f0736fc9b366a31735542662d3d0
Filip M <filip.moszczynski@onet.pl>
2020-09-05 10:32:58 +0200

    Initial commit

---
+++ src/main/java/pl/sda/dto/Location.java
@@ -0,0 +1,5 @@
+package pl.sda.dto;
+
+public class Location {
+
+}
---
+++ src/main/java/pl/sda/view/LocationManager.java
@@ -0,0 +1,12 @@
+package pl.sda.view;
+
+import pl.sda.dto.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationManager {
+    private List<Location> locations = new ArrayList<>();
+
+
+}
